d62e9012a1eccb44527a71c0370c6bb36fbc4c2f
mohd zeeshan <mohdzeeshan7775@gamil.com>
2022-06-13 21:50:19 +0530

    commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,22 @@ import '../styles/App.css';
 const App = () => {
 
 const[text,setText] = useState();
+const[word,setWord] = useState();
 const[limit,setLimit] = useState(50);
-console.log(text);
+const[size,setSize] = useState(16);
+console.log(size);
+// console.log(text);
+// let s = size;
 // let txt = text.length;
   return (
     <>
     <div className='container'>
        <h3>Font size Picker</h3>
-      <input type="range" id="vol" name="vol" min="0" max="50"></input>
+      <input type="range" id="vol" name="vol" min="0" max="50"
+      onChange={function(e){
+        // console.log(e.target.value);
+        setSize(e.target.value)
+      }}></input>
     </div>
     <div className='container'>
        <h3>Word limit input</h3>
@@ -20,11 +28,15 @@ console.log(text);
       }}/>
     </div>
     <div className='container'>
-      <textarea maxLength={50} onChange={function(e){setText(e.target.value)}}></textarea>
+      <textarea maxLength={limit} placeholder="enter your words" style={{fontSize:(Number)(size)}}
+      onChange={function(e){
+        setText(e.target.value.length);
+        setWord(e.target.value.split(" ").length)
+        }}></textarea>
     </div>
 
-    <div id="word-counter" className='container'>Total number of words written </div>
-    <div id="char-counter" className='container'>Total number of characters used </div>
+    <div id="word-counter" className='container'>Total number of words written {text}</div>
+    <div id="char-counter" className='container'>Total number of characters used {word}</div>
     </>
   )
 }
