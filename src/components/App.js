3562e4c2cb8be8c166169e133e124af030865ff1
Mohd Zeeshan <100269606+MohdZeeshan11@users.noreply.github.com>
2022-06-15 09:08:33 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,42 +2,47 @@ import React, { useState } from 'react'
 import '../styles/App.css';
 
 const App = () => {
+  const [fontSize,setFontSize] = useState(16);
+  const [charLimit,setCharLimit] = useState(50);
+  const [charCount,setCharCount] = useState(); 
+  const [wordCount,setWordCount] = useState(); 
 
-const[text,setText] = useState();
-const[word,setWord] = useState();
-const[limit,setLimit] = useState(50);
-const[size,setSize] = useState();
-console.log(size);
-// console.log(text);
-// let s = size;
-// let txt = text.length;
+  const inputChangeHandler = (e)=>{
+        console.log(e.target.value);
+        const text = e.target.value;
+
+        // set word count
+        let w_count = 0;
+        if(text.length>0){
+          w_count = text.trim().split(" ").length;
+        }
+        setWordCount(w_count);
+
+        // set char count
+        setCharCount(text.length);
+  } 
   return (
-    <>
-    <div className='container'>
-       <h3>Font size Picker</h3>
-      <input type="range" id="vol" name="vol" min="0" max="50"
-      onChange={function(e){
-        // console.log(e.target.value);
-        setSize(e.target.value)
-      }}></input>
-    </div>
-    <div className='container'>
-       <h3>Word limit input</h3>
-      <input type="number" value={limit} onChange={function(){
-        setLimit(limit+1);
-      }}/>
-    </div>
-    <div className='container'>
-      <textarea maxLength={limit} placeholder="enter your words" style={{fontSize:(Number)(size)}}
-      onChange={function(e){
-        setText(e.target.value.length);
-        setWord(e.target.value.split(" ").length)
-        }}></textarea>
-    </div>
+    <div id="main">
+      <h3>Font size picker</h3>
+      <input type="range" min="16" max="32" id="fontSize-input" 
+        onChange={function(e){
+          setFontSize(e.target.value);
+        }}
+      />
 
-    <div id="word-counter" className='container'>Total number of words written {word}</div>
-    <div id="char-counter" className='container'>Total number of characters used {text}</div>
-    </>
+      <h3>Word limit input</h3>
+      <input type="number"  id="char-limit-input" value={charLimit}
+        onChange={function(e){
+          setCharLimit(e.target.value);
+        }}
+      />
+      <br />
+      <textarea style={{fontSize:fontSize+"px"}} maxLength={charLimit}
+      onChange={inputChangeHandler}></textarea>
+
+      <div id="word-counter">Total number of words written {wordCount}</div>
+      <div id="char-counter">Total number of characters used {charCount}</div>
+    </div>
   )
 }
 
